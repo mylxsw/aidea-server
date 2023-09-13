e562093f3343c1529eb04cf43512e5089309f5d7
管宜尧 <mylxsw@aicode.cc>
2023-09-13 10:56:52 +0800

    update docker build scripts

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 VERSION=1.0.4
+VERSION_DATE=202309131051
 
 docker build -t mylxsw/aidea-server:$VERSION .
-#docker push mylxsw/aidea-server:$VERSION
+docker tag mylxsw/aidea-server:$VERSION mylxsw/aidea-server:$VERSION_DATE
+docker tag mylxsw/aidea-server:$VERSION mylxsw/aidea-server:latest
+
+docker push mylxsw/aidea-server:$VERSION
+docker push mylxsw/aidea-server:$VERSION_DATE
+docker push mylxsw/aidea-server:latest
+
