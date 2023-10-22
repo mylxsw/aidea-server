f4ae47d7d73202495c4db74f5b0543bc6dd1e814
mylxsw <mylxsw@aicode.cc>
2023-10-23 00:50:31 +0800

    update

--- internal/queue/getimgai.go
+++ internal/queue/getimgai.go
@@ -344,7 +344,11 @@ func resolvePrompts(ctx context.Context, payload PromptResolverPayload, creative
 		}
 	}
 
-	if negativePrompt == "" {
+	if strings.TrimSpace(prompt) == "" {
+		prompt = "best quality, animation effect"
+	}
+
+	if strings.TrimSpace(negativePrompt) == "" {
 		negativePrompt = "out of frame, lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature"
 	}
 
