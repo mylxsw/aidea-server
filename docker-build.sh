5a90fddf68aa2a417eaed3484b54b238778969e7
mylxsw <mylxsw@aicode.cc>
2023-11-29 17:46:21 +0800

    update

--- docker-build.sh
+++ docker-build.sh
@@ -2,5 +2,5 @@
 
 VERSION=1.0.9
 
-docker buildx build --platform=linux/amd64,linux/arm64,linux/arm/v7 -t mylxsw/aidea-server:$VERSION . --push
+docker buildx build --platform=linux/amd64,linux/arm64 -t mylxsw/aidea-server:$VERSION . --push
 
